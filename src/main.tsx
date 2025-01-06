34771708d7470465fc92ca21604199d78bc49d8d
Sawom <asawom250@gmail.com>
2025-01-06 21:56:29 +0600

    redux added

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes/routes.tsx";
+import { store } from "./routes/store.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    
-    <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
