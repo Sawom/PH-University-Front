b79501d9b636d8dbb1604830357ca128269997f7
Sawom <asawom250@gmail.com>
2025-02-14 18:00:53 +0600

    auth api

--- src/redux/Features/auth/authApi.ts
+++ src/redux/Features/auth/authApi.ts
@@ -1,2 +1,18 @@
 // erver er sathe communications
 // auth er jnno alada api
+
+import { baseApi } from "../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
