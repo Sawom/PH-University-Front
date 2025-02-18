fa93b63f43a9e9f4b359c7e43ab76db35d66bdb8
Sawom <asawom250@gmail.com>
2025-02-19 02:17:18 +0600

    updated

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,38 +1,43 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import { routesGenerator } from '../utils/routesGenerator';
-import { adminPaths } from './admin.routes';
-import facultyPaths from './faculty.routes';
-import studentPaths from './student.routes';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import ProtectedRoute from "../components/layout/ProtectedRoute";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { routesGenerator } from "../utils/routesGenerator";
+import { adminPaths } from "./admin.routes";
+import facultyPaths from "./faculty.routes";
+import studentPaths from "./student.routes";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
   },
   {
-    path: '/admin',
-    element: <App />,
-    children: routesGenerator(adminPaths) ,
+    path: "/admin",
+    element: (
+      <ProtectedRoute>
+        <App />
+      </ProtectedRoute>
+    ),
+    children: routesGenerator(adminPaths),
   },
   {
-    path: '/faculty',
+    path: "/faculty",
     element: <App />,
     children: routesGenerator(facultyPaths),
   },
   {
-    path: '/student',
+    path: "/student",
     element: <App />,
     children: routesGenerator(studentPaths),
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
 ]);
